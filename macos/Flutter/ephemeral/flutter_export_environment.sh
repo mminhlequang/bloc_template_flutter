eeaaec6d5653dc1b3070246cfecd9333b91005a2
Minh Minh <mminh.lequang@gmail.com>
2024-06-16 14:16:51 +0700

    -

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mminh/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mminh/Documents/flutter_workspace/iwu/bloc_template_flutter"
+export "FLUTTER_ROOT=C:\Users\WIN\Documents\development\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Documents\Workspace\mminhlequang\bloc_template_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.1"
-export "FLUTTER_BUILD_NUMBER=1.0.1"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
